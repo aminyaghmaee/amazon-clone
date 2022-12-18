d5824f21cb81105ad8a27e00ad7f11f3eff8f66f
amin <yaghmaeeamin@yahoo.com>
2022-12-18 08:53:25 +0330

    header and navbar fixed

--- src/components/Header/NavBar.jsx
+++ src/components/Header/NavBar.jsx
@@ -4,35 +4,34 @@ import "./NavBar.css";
 import {Link } from 'react-router-dom';
 export default function NavBar() {
   return (
-    <div className='nav_main'>
-      <div className='nav_left'>
-        <DehazeIcon/>
-        <span className="nav_left_span">
-          All
-        </span>
-      </div>
-      <div className='nav_fill'>
-         <Link to="/todaysdeals">
-          <div className="links_container">Today's Deals</div>
-         </Link>
-         <Link to="/customerservice">
-          <div className="links_container">Customer Service</div>
-         </Link>
-         <Link to="/registery">
-          <div className="links_container">Registry</div>
-         </Link>
-         <Link to="/giftcards">
-          <div className="links_container">Gift Cards</div>
-         </Link>
-         <Link to="/sell">
-          <div className="links_container">Sell</div>
-         </Link>
+    <div className="nav_main">
+      <div className="nav_left">
+        <div className="first_container">
+          <DehazeIcon />
+          <div className="first_links_container">All</div>
         </div>
-      <div className='nav_right'>
-         <Link to="/">
-          <div className="links_container">Shop deals in Electronics</div>
-         </Link>
+        <Link to="/todaysdeals">
+          <div className="links_container">Today's Deals</div>
+        </Link>
+        <Link to="/customerservice">
+          <div className="links_container">Customer Service</div>
+        </Link>
+        <Link to="/registery">
+          <div className="links_container">Registry</div>
+        </Link>
+        <Link to="/giftcards">
+          <div className="links_container">Gift Cards</div>
+        </Link>
+        <Link to="/sell">
+          <div className="links_container">Sell</div>
+        </Link>
+      </div>
+
+      <div className="nav_right">
+        <Link to="/">
+          <div className="last_links_container">Shop deals in Electronics</div>
+        </Link>
       </div>
     </div>
-  )
+  );
 }
